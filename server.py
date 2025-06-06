def856a6c1ca0272fa54ee812fb6ffb8c0486b34
divinejoshua <divineuche399@gmail.com>
2025-06-06 23:47:09 +0100

    Added markdown converter tool

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
 # server.py
+import os
+import requests
 from mcp.server.fastmcp import FastMCP
 
 # Create an MCP server
@@ -11,6 +13,21 @@ def add(a: int, b: int) -> int:
     """Add two numbers"""
     return a + b
 
+@mcp.tool()
+def markdown_converter(url: str) -> str:
+    """Convert website to markdown"""
+    response = requests.post(
+        "https://api.wetrocloud.com/v2/markdown-converter/",
+        headers={
+            "Authorization": f"Token {os.getenv('WETRO_API_KEY')}",
+            "Content-Type": "application/json",
+        },
+        json={
+            "link": url,
+            "resource_type": "web",
+        },
+    )
+    return response.json()
 
 # Add a dynamic greeting resource
 @mcp.resource("greeting://{name}")
