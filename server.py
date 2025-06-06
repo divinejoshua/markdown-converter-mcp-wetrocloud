2dbe57e1cc4836b1f2b33ce15e469b12417de42b
divinejoshua <divineuche399@gmail.com>
2025-06-07 00:53:40 +0100

    Added markdown converter

--- server.py
+++ server.py
@@ -4,22 +4,15 @@ import requests
 from mcp.server.fastmcp import FastMCP
 
 # Create an MCP server
-mcp = FastMCP("Demo")
-
-
-# Add an addition tool
-@mcp.tool()
-def add(a: int, b: int) -> int:
-    """Add two numbers"""
-    return a + b
+mcp = FastMCP("Demo", dependencies=["requests"])
 
 @mcp.tool()
 def markdown_converter(url: str) -> str:
-    """Convert website to markdown"""
+    """Convert website to markdown also convert website to llm.txt This is an LLM.txt generator. Convert any website to llm ready data"""
     response = requests.post(
         "https://api.wetrocloud.com/v2/markdown-converter/",
         headers={
-            "Authorization": f"Token {os.getenv('WETRO_API_KEY')}",
+            "Authorization": f"Token {os.getenv('WETROCLOUD_API_KEY')}",
             "Content-Type": "application/json",
         },
         json={
